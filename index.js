b8c52ce6b054e668734e68005e05d1ce90d3cc0a
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-04-23 23:52:55 +0200

    user mentions + arguments

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Discord.Client({
+  partials: ["MESSAGE"],
+});
 
 const prefix = "!";
 
@@ -10,6 +12,10 @@ client.once("ready", () => {
 });
 
 client.on("message", (msg) => {
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+  const args = msg.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
   if (msg.content.startsWith(`${prefix}ping`)) {
     msg.channel.send("pong");
   } else if (msg.content.startsWith(`${prefix}beep`)) {
@@ -22,6 +28,22 @@ client.on("message", (msg) => {
     msg.channel.send(
       `Your username: ${msg.author.username}\nYour ID: ${msg.author.id}`
     );
+  } else if (command === "args-info") {
+    if (!args.length) {
+      return msg.channel.send(
+        `You didn't provide any arguments, ${msg.author}!`
+      );
+    } else if (args[0] === "foo") {
+      return msg.channel.send("fighters");
+    }
+
+    msg.channel.send(`Command name: ${command}\nFirst Argument: ${args[0]}`);
+  } else if (command === "kick") {
+    if (!msg.mentions.users.size) {
+      return msg.reply("You need to tag a user in order to kick them!");
+    }
+    const taggedUser = msg.mentions.users.first();
+    msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
   }
 });
 
