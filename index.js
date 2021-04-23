9239ff860946c8e8b3752ff2a847ac607453b96b
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-04-23 23:24:47 +0200

    some basic commands

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on("ready", () => {
+const prefix = "!";
+
+client.once("ready", () => {
   console.log("Botty the bot is ready to work!");
+});
+
+client.on("message", (msg) => {
+  if (msg.content.startsWith(`${prefix}ping`)) {
+    msg.channel.send("pong");
+  } else if (msg.content.startsWith(`${prefix}beep`)) {
+    msg.channel.send("boop");
+  } else if (msg.content === `${prefix}server`) {
+    msg.channel.send(
+      `This server's name is ${msg.guild.name}\nTotal members: ${msg.guild.memberCount}`
+    );
+  } else if (msg.content === `${prefix}my-info`) {
+    msg.channel.send(
+      `Your username: ${msg.author.username}\nYour ID: ${msg.author.id}`
+    );
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
